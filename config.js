9e0858df013e3c570f9e0d74ae3d9668d16fd48d
azimSpeedum <ababu@speedum.co.in>
2022-08-17 11:47:15 +0530

    init

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  twilio: {
+    accountSid: "AC23a185937fe9f0885ef9427e45f4a8fb",
+    apiKey: "SKd074e8291e28ab65896d95cbfe66a661",
+    apiSecret: "1tiJXyemghKhTX3Tj4w7Hhy6FCwdokXw",
+    outgoingApplicationSid: "APf8615a03ea439975a84d07c40607586f",
+    incomingAllow: "true",
+    callerId: "+1 251 551 9578"
+  }
+};
