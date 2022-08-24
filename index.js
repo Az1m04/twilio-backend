178ef87258a1e8c0737891bff784d6e08d6fe56e
Md Azim Babu <42888008+Az1m04@users.noreply.github.com>
2022-08-24 18:03:15 +0530

    voice rate

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.all("/results", (req, res) => {
    default:
       response.say("Sorry, I don't undersatand that choice.");
      const gather=response.gather({input:'dtmf'});
-     gather.say({ voice: 'alice' }).prosody({rate: '85%'},"Thank you for calling Health Vault. Please dial the extension if you know or dial 0 to talk to our agent.")
+     gather.say({ voice: 'alice' }).prosody({rate: '60%'},"Thank you for calling Health Vault. Please dial the extension if you know or dial 0 to talk to our agent.")
      break;  
 }
 res.send(response.toString());
