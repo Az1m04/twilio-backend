2f2eb3facc5ee6b192f178de67df6098bc15fd8f
Md Azim Babu <42888008+Az1m04@users.noreply.github.com>
2022-08-25 10:30:37 +0530

    .

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const sendTokenResponse = (token, res) => {
 
 app.post("/voice/token", (req, res) => {
   const identity = req.body.identity;
+
   const token = voiceToken(identity, config);
   sendTokenResponse(token, res);
 });
@@ -56,7 +57,7 @@ app.get("/voice/token", (req, res) => {
 });
 
 app.post("/voice/incoming", (req, res) => {
-  console.log(ID,req.body.identity)
+  console.log(  ID,">>> ID",req.body.identity,">>>> INDETIDY")
   const response = new VoiceResponse();
   response.say({voice:'alice'},"Thank you for calling Health Vault.")
   response.pause({length:2})
@@ -74,7 +75,7 @@ app.all("/results", (req, res) => {
   const userInput = req.body.Digits;
   const response = new VoiceResponse();
   const dial = response.dial({ callerId: req.body.From, answerOnBridge: true });
-   switch (req.body.Digits){
+ switch (req.body.Digits){
    case '0':
      response.say('You have dialed 0.');
      dial.client(ID);
