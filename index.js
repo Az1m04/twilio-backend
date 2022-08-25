f77a983ecafad86f419030591ea0bc3257b2b217
Md Azim Babu <42888008+Az1m04@users.noreply.github.com>
2022-08-25 10:08:10 +0530

    .

--- index.js
+++ index.js
@@ -70,14 +70,13 @@ const gather=response.gather({
 app.all("/results", (req, res) => {
   const userInput = req.body.Digits;
   const response = new VoiceResponse();
+  const dial = response.dial({ callerId: req.body.From, answerOnBridge: true });
  switch (req.body.Digits){
    case '0':
      response.say('You have dialed 0.');
-     const dial = response.dial({ callerId: req.body.From, answerOnBridge: true });
      dial.client('18');
      break;
      case '100':
-      const dial = response.dial({ callerId: req.body.From, answerOnBridge: true });
       dial.client('17');
      break;
    default:
