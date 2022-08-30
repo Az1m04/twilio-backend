b0193f930acfa8017460c5777d89ce37f8269bae
azimSpeedum <ababu@speedum.co.in>
2022-08-30 11:38:19 +0530

    .

--- index.js
+++ index.js
@@ -5,24 +5,26 @@ const { voiceToken } = require("./tokens");
 const { VoiceResponse } = require("twilio").twiml;
 const cors = require("cors");
 const app = express();
-var onlineClients=[];
-var callAnsered=false;
+var onlineClients = []; //store available online clients
 
-var allowedDomains = ['https://dev-01.speedum.tech', 'http://localhost:3000'];
-app.use(cors({
-  origin: function (origin, callback) {
-    // bypass the requests with no origin (like curl requests, mobile apps, etc )
-    if (!origin) return callback(null, true);
- 
-    if (allowedDomains.indexOf(origin) === -1) {
-      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+var allowedDomains = ["https://dev-01.speedum.tech", "http://localhost:3000"]; //allowed domains
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // bypass the requests with no origin (like curl requests, mobile apps, etc )
+      if (!origin) return callback(null, true);
+
+      if (allowedDomains.indexOf(origin) === -1) {
+        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 const sendTokenResponse = (token, res) => {
   res.set("Content-Type", "application/json");
   res.send(
@@ -38,6 +40,8 @@ app.post("/voice/token", (req, res) => {
   sendTokenResponse(token, res);
 });
 
+/***************** HANDLE OUTGOING CALL***************** */
+/***********************STARTS******************************/
 app.post("/voice", (req, res) => {
   const To = req.body.To;
   const response = new VoiceResponse();
@@ -46,179 +50,232 @@ app.post("/voice", (req, res) => {
   res.set("Content-Type", "text/xml");
   res.send(response.toString());
 });
+/***********************ENDS******************************/
 
+/***************** HANDLE CLIENT ONLINE STATE ***************** */
+/***********************STARTS******************************/
 app.get("/voice/token", (req, res) => {
-  const identity = req.query.identity;
-  onlineClients.push(identity)
-  const unique= [...new Set(onlineClients?.map(v=>v))]
-  onlineClients=unique
-  const token = voiceToken(identity, config);
-  sendTokenResponse(token, res);
+  const identity = req.query.identity; // online client identity
+  onlineClients.push(identity);  //pushing it to available clients array
+  const unique = [...new Set(onlineClients?.map((v) => v))]; //removing the duplicay of client online 
+  onlineClients = unique;
+  const token = voiceToken(identity, config); //Genrating token
+  sendTokenResponse(token, res); //sending the token response
 });
+/***********************ENDS******************************/
 
+/***************** HANDLE CLIENT OFFLINE STATE ***************** */
+/***********************STARTS******************************/
 app.get("/voice/removetoken", (req, res) => {
-  const identity = req.query.identity;
-  const arr = onlineClients?.filter((item)=> {
-    return item !== identity
-   })
-  onlineClients=arr
+  const identity = req.query.identity;  // offline client identity
+  const arr = onlineClients?.filter((item) => { 
+    return item !== identity; //removing the client id  from available clint array if client device is OFFLINE
+  });
+  onlineClients = arr;
   res.send({
-    returnCode: "true"
-  })
+    returnCode: "true",
+  });
 });
+/***********************ENDS******************************/
 
+/***************** HANDLE INCOMING CALL ***************** */
+/***********************STARTS******************************/
 app.post("/voice/incoming", (req, res) => {
   const response = new VoiceResponse();
-  response.say({voice:'alice'},"Thank you for calling Health Vault.")
-  response.pause({length:2})
-  const gatherValue=()=>{
-    const gather=response.gather({
-      input:'dtmf',
-      action:'/results',
-      timeout: 'auto',
-      })
-      const say=gather.say({ voice: 'alice' })
-      say.prosody({
-        rate: 'x-slow',
-    }, "Please dial the extension if you know or dial 0 to talk to our agent.");
-  }
-  gatherValue()
-  response.say('You have not daial any input. Please try again.')
-  gatherValue()
-  response.pause()
-  response.say('Thanks for calling.')
+  response.say({ voice: "alice" }, "Thank you for calling Health Vault.");
+  response.pause({ length: 2 });
+
+  //Gather input from the user
+  const gatherValue = () => {
+    const gather = response.gather({
+      input: "dtmf",
+      action: "/results",
+      timeout: "auto",
+    });
+    const say = gather.say({ voice: "alice" });
+    say.prosody(
+      {
+        rate: "x-slow", // voice speed
+      },
+      "Please dial the extension if you know or dial 0 to talk to our agent."
+    );
+  };
+
+  gatherValue(); // gathering from user
+
+  response.say("You have not daial any input. Please try again.");
+  
+  gatherValue();  // Retry gathering from user
+  
+  response.pause();
+  
+  response.say("Thanks for calling."); //IF not input then calls  end
+  
   res.set("Content-Type", "text/xml");
   res.send(response.toString());
 });
+/***********************ENDS******************************/
 
+
+/***************** HANDLE RESULTS OF GATHERD USERINPUT ***************** */
+/***********************STARTS******************************/
 app.post("/results", (req, res) => {
-  const userInput = req.body.Digits;
+  const userInput = req.body.Digits;  // user input value
   const response = new VoiceResponse();
-  const dial = response.dial({ callerId: req.body.From, answerOnBridge: true,timeout:10,action:`/handleDialCallStatus?dialInput=${userInput}`});
-  const gatherValue=()=>{
-    const gather=response.gather({
-      input:'dtmf',
-      action:'/results',
-      timeout: 'auto',
-      })
-      const say=gather.say({ voice: 'alice' })
-      say.prosody({
-        rate: 'x-slow',
-    }, "Please dial the extension if you know or dial 0 to talk to our agent.");
-  }
+  const dial = response.dial({
+    callerId: req.body.From,  // getting call from user
+    answerOnBridge: true,
+    timeout: 10, // dial timeout in seconds
+    action: `/handleDialCallStatus?dialInput=${userInput}`,  // dial call action handler
+  });
+  const gatherValue = () => {
+    const gather = response.gather({
+      input: "dtmf",
+      action: "/results",
+      timeout: "auto",
+    });
+    const say = gather.say({ voice: "alice" });
+    say.prosody(
+      {
+        rate: "x-slow",
+      },
+      "Please dial the extension if you know or dial 0 to talk to our agent."
+    );
+  };
 
-
-
-  const callFallback=()=>{
-    const gather=response.gather()
-    gather.say({ voice: 'alice' },"Sorry, no one is available to take your call. Please leave a message at the beep.\nPress the star key when finished.")
+  //Voicemail response callback
+  const callFallback = () => {
+    const gather = response.gather();
+    gather.say(
+      { voice: "alice" },
+      "Sorry, no one is available to take your call. Please leave a message at the beep.\nPress the star key when finished."
+    );
     response.record({
       action: "/voicemail",
       playBeep: true,
-      finishOnKey: '*'
-     });
-   }
+      finishOnKey: "*",
+    });
+  };
 
-    switch (req.body.Digits){
-      case '0':
-        if(onlineClients?.includes('15')){
-          dial.client('15')
-       }
-       else {
-        callFallback()
-       }     
-       break;
-       case '100':
-        if(onlineClients?.includes('18')){
-          dial.client('18')
-          }
-        else {
-          callFallback()
-         }
-        break;
-      default:
-         response.say("Sorry, I don't undersatand that choice.");  
-         response.say("Please try again."); 
-         response.pause({length:1}) 
-         gatherValue()
-       break;
-    }
-res.send(response.toString());
+
+//Gather digit output results
+  switch (req.body.Digits) {
+    case "0":
+      if (onlineClients?.includes("15")) {
+        dial.client("15");
+      } else {
+        callFallback();
+      }
+      break;
+    case "100":
+      if (onlineClients?.includes("18")) {
+          dial.client("18");
+      } else {
+        callFallback();
+      }
+      break;
+    default:
+      response.say("Sorry, I don't undersatand that choice.");
+      response.say("Please try again.");
+      response.pause({ length: 1 });
+      gatherValue();
+      break;
+  }
+  res.send(response.toString());
 });
-app.post("/handleDialCallStatus", (req, res) => {
-  console.log(req?.body,">>>>> STATUS")
-  const callerIdFallback=req?.query?.dialInput
-  console.log(callerIdFallback,">>>>> INPUT")
-  const response = new VoiceResponse();
-  const badStatusCodes=["busy",
-    "no-answer",
-    "canceled",
-    "failed"]
-    if (!badStatusCodes.includes(req.body.DialCallStatus))
-    { 
-     return  res.send(response.toString())
-    }
-    
-    if(callerIdFallback<=0){
-      response.redirect(`/handleRedirect?clientId=${callerIdFallback}`)
-    }
-    else {
-      const gather=response.gather()
-      gather.say({ voice: 'alice' },"Sorry, no one is available to take your call. Please leave a message at the beep.\nPress the star key when finished.")
-      response.record({
-        action: "/voicemail",
-        playBeep: true,
-        finishOnKey: '*'
-       });
-    }
+/***********************ENDS******************************/
 
+/***************** HANDLE DIAL CALL BACK  ***************** */
+/***********************STARTS******************************/
+app.post("/handleDialCallStatus", (req, res) => {
+
+  const callerIdFallback = req?.query?.dialInput;       // dialed input 
+
+  const response = new VoiceResponse();
+  
+  const badStatusCodes = ["busy", "no-answer", "canceled", "failed"]; //Bad call cases
+
+  if (!badStatusCodes.includes(req.body.DialCallStatus)) {
+    return res.send(response.toString());
+  }
+
+  if (callerIdFallback <= 0) {
+    response.redirect(`/handleRedirect?clientId=${callerIdFallback}`);   // redirect call if dialed input 0
+  } else {
+     // Record voicemail if client not available
+    const gather = response.gather();
+    gather.say(
+      { voice: "alice" },
+      "Sorry, no one is available to take your call. Please leave a message at the beep.\nPress the star key when finished."
+    );
+    response.record({
+      action: "/voicemail",
+      playBeep: true,
+      finishOnKey: "*",
+    });
+  }
 
   res.set("Content-Type", "text/xml");
-  res.send(response.toString())
+  res.send(response.toString());
 });
+/***********************ENDS******************************/
 
+
+
+/***************** HANDLE REDIRECT CALL ***************** */
+/***********************STARTS******************************/
 app.post("/handleRedirect", (req, res) => {
-  const callerIdFallback=req?.query?.clientId
+  const callerIdFallback = req?.query?.clientId;  //Client Id who did not answer the call
   const response = new VoiceResponse();
-  const updateClient= onlineClients?.filter((item)=> {
-    return item !== callerIdFallback
-   })
 
-  const dial = response.dial({ callerId: req.body.From, answerOnBridge: true,timeout:10});
-  const random= updateClient[Math.floor(Math.random()*updateClient.length)];
-  if(updateClient?.length>0){
-     dial.client(random)
+  //Removeing the client whot did not answer the call from available list 
+  const updateClient = onlineClients?.filter((item) => {
+    return item !== callerIdFallback;
+  });
+
+  const dial = response.dial({
+    callerId: req.body.From,
+    answerOnBridge: true,
+    timeout: 10,
+  });
+
+  const random = updateClient[Math.floor(Math.random() * updateClient.length)]; // Taking random available client
+  if (updateClient?.length > 0) {
+
+    dial.client(random);    // Redial to available clients
+  } else {
+       // Record voicemail if client not available
+    const gather = response.gather();
+    gather.say(
+      { voice: "alice" },
+      "Sorry, no one is available to take your call. Please leave a message at the beep.\nPress the star key when finished."
+    );
+    response.record({
+      action: "/voicemail",
+      playBeep: true,
+      finishOnKey: "*",
+    });
   }
-  else {
-      const gather=response.gather()
-      gather.say({ voice: 'alice' },"Sorry, no one is available to take your call. Please leave a message at the beep.\nPress the star key when finished.")
-      response.record({
-        action: "/voicemail",
-        playBeep: true,
-        finishOnKey: '*'
-       });
-     
-  }
-  res.send(response.toString())
+  res.send(response.toString());
 });
 
+/***********************ENDS******************************/
 
-app.post("/calls/events", (req, res) => {
-  const response = new VoiceResponse();
-  res.send(response.toString())
-});
 
-app.all("/voicemail",(req,res)=>{
+/***************** HANDLE VOICEMAIL ***************** */
+/***********************STARTS******************************/
+
+app.all("/voicemail", (req, res) => {
   const response = new VoiceResponse();
-  response.say("Thank you for your message. Good bye.");
+
+  response.say("Thank you for your message. Good bye.");  // recieved voice response message
   response.hangup();
-  res.send(response.toString())
-})
+  res.send(response.toString());
+});
 
+/***********************ENDS******************************/
 
-
-
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 8888;   //SET the server port
 
 app.listen(port, () =>
   console.log(`Express server is running on localhost:${port}`)
